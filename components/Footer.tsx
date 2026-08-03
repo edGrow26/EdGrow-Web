14b2a5ff71e8807867deeb952d8ba0c9ea6aede9
Keerthi Keerthi <keerthihan05@gmail.com>
2026-08-03 15:09:23 +0530

    git account switch and push

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Mail, Phone, MapPin, Linkedin, Github, Twitter, ExternalLink } from 'lucide-react';
+// Components
 import ScrollAnimate from './ScrollAnimate';
 
 export default function Footer() {
