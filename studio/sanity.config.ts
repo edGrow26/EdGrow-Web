0fb49140071a7a496bb13aa6873ba2e474bbe347
jeeviethofficial-crypto <jeevieth.official@gmail.com>
2026-08-04 11:03:28 +0530

    add sanity fix

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,8 +3,8 @@ import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemaTypes';
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
-const dataset = process.env.SANITY_STUDIO_DATASET || 'production';
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
 if (!projectId || projectId === 'your-project-id') {
   throw new Error(
