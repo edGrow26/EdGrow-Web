b6df7b9fd757209e1aa4887deb8082fe3fe8c7b2
Edgrow <edgrowproduct@gmail.com>
2026-08-06 09:26:44 +0500

    Uncomment TeamPage implementation and set dynamic

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,6 +1,20 @@
+// import TeamPageClient from '../../components/TeamPageClient';
+// import { sanityClient as mockSanityClient } from '../../lib/sanity';
+// import { fetchSanityTeam } from '../../sanity/lib/team';
+
+// export default async function TeamPage() {
+//   const fallbackTeam = await mockSanityClient.getTeam();
+//   const team = await fetchSanityTeam(fallbackTeam);
+
+//   return <TeamPageClient team={team} />;
+// }
+
+
 import TeamPageClient from '../../components/TeamPageClient';
 import { sanityClient as mockSanityClient } from '../../lib/sanity';
 import { fetchSanityTeam } from '../../sanity/lib/team';
+
+export const dynamic = 'force-dynamic';
 
 export default async function TeamPage() {
   const fallbackTeam = await mockSanityClient.getTeam();
